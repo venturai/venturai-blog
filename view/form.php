611471c3610e26ebbc84dc12e37e6a097878ad0e
isaura ventura <isaura.ventura@yahoo.com>
2015-02-10 15:01:08 -0800

    establishing a database connection

--- view/form.php
+++ view/form.php
@@ -1,6 +1,10 @@
+<? php
+     require_once(__DIR__ . "/../model/config.php");
+?>
+
 <h1>Create Blog Post</h1>
 
-<form>
+<form method="post" action="<?php echo $path . "controller/create.php"; ?>">
     <div>    
         <label for="title">Title: </label>
         <input type="text" name="title" />

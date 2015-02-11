9722bd7c393559ec32ad4242bb3fc7cf140c5180
isaura ventura <isaura.ventura@yahoo.com>
2015-02-11 15:03:29 -0800

    refactoring the configuartion code

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,17 @@
 <?php
+    require_once(__DIR__ . "/../model/database.php");
+    
+    $connection = new mysqli($host, $username, $password, $database);
+    
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING); 
 
-    echo "<p>Title: $title</p>";
-    echo "<p>Post: $post</p>";
+    $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+    
+    if($query) {
+        echo "<p>Successfully inserted post: $title</p>";
+    }
+    else {
+      echo "<p>$connection->error</p>";  
+    }
+    $connection->close();

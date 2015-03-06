4b722be4a20678416fe060e4d6a1391dce777925
isaura ventura <isaura.ventura@yahoo.com>
2015-03-06 14:36:22 -0800

    no message

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-    require_once(__DIR__ . "/controller/login-verfiy.php");
+    require_once(__DIR__ . "/controller/login-verify.php");
     require_once(__DIR__ . "/view/header.php");
     if(authenticateUser()) {
         require_once(__DIR__ . "/view/navagation.php");
     }
     require_once(__DIR__ . "/controller/create-db.php");
     require_once(__DIR__ . "/view/footer.php");
-    require_once(__DIR__ . "/contoller/read-posts.php")
+    require_once(__DIR__ . "/controller/read-posts.php");
 ?>
 
--- model/config.php
+++ model/config.php
@@ -1,7 +1,6 @@
 <?php
     require_once(__DIR__ . "/Database.php");
     session_start();
-    session_regener_id(true);
     $path = "/venturai-blog/";
     
     $host = "localhost";
